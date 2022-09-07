e441f2582202b8ae3b17c9b81933dff41ae05895
Mattias Söderberg <mattes.email@gmail.com>
2022-09-07 18:35:33 +0200

    Add categories and tags

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ function App() {
 
   useEffect(() => {
     const url = process.env.REACT_APP_API_URL
-    console.log(url)
 
     fetch(url)
       .then(res => res.json())
       .then(data => setPosts(data.posts))
   }, [])
-
+  
   return (
     <div>
       <h1>Home</h1>
@@ -19,6 +18,18 @@ function App() {
         return <div key={post.ID}>
           <h2>{post.title}</h2>
           <div dangerouslySetInnerHTML={ {__html: post.content} } />
+          <div>
+            <h4>Kategorier</h4>
+            {Object.entries(post.terms.category).map(cat => {
+              return <p key={cat[1].ID}>{cat[1].name}</p>
+            })}
+          </div>
+          <div>
+            <h4>Taggar</h4>
+            {Object.entries(post.tags).map(tag => {
+              return <p key={tag[1].ID}>{tag[1].name}</p>
+            })}
+          </div>
         </div>
       })}
     </div>
