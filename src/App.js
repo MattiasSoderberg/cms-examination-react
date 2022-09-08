637abeb7af65cbc36acbf99209f7e65d5ce54cb2
Mattias Söderberg <mattes.email@gmail.com>
2022-09-08 10:33:03 +0200

    Add PostDetailPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react"
 import { Routes, Route } from "react-router-dom"
+import PostDetailPage from "./pages/PostDetailPage";
 import PostsListPage from "./pages/PostsListPage";
 
 function App() {
   
   return (
     <div>
+      <h1>Mattias Söderberg</h1>
       <Routes>
         <Route path="/" element={<PostsListPage />} />
+        <Route path="/:id" element={<PostDetailPage />} />
       </Routes>
     </div>
   );
